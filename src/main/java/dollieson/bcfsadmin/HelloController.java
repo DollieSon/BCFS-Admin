abf9fff2ad81d46591477e636581330bf46e7f76
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-05-20 00:25:55 +0800

    added Attack Creation

--- src/main/java/dollieson/bcfsadmin/HelloController.java
+++ src/main/java/dollieson/bcfsadmin/HelloController.java
@@ -12,6 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
@@ -28,9 +29,9 @@ public class HelloController implements Initializable {
     public ChoiceBox cbAttackMod;
     public Button btnSave;
     public String choices[];
-    public GridPane gpAttackCont;
+    public ScrollPane spContainer;
 
-    private int contcol = 5;
+    private int contcol = 4;
 
 
     @FXML
@@ -39,11 +40,11 @@ public class HelloController implements Initializable {
         int speed = Integer.parseInt(tfSpeed.getText());
         int damage= Integer.parseInt(tfDamage.getText());;
         double damageMult= Double.parseDouble(tfDamageMult.getText());
-        AttackModule AtkMod =AttackModuleBuilder.buildAttackModule(cbAttackMod.getSelectionModel().getSelectedIndex());
+        AttackModule AtkMod =AttackModuleBuilder.buildAttackModule(cbAttackMod.getSelectionModel().getSelectedIndex()+1);
         System.out.println(String.format("%s %d %d %f", Name,speed,damage,damageMult));
         Attack atk = new Attack(Name,speed,damage,damageMult,AtkMod);
         DBHelpers dbh = new DBHelpers(DBHelpers.getGlobalConnection());
-        //insert
+        dbh.sendAttack(atk);
         RepopulateTable();
     }
 
@@ -64,7 +65,7 @@ public class HelloController implements Initializable {
     }
     public void RepopulateTable(){
         System.out.println("repopulating.....");
-        gpAttackCont.getChildren().clear();
+        GridPane gp = new GridPane();
         DBHelpers dbh = new DBHelpers(DBHelpers.getGlobalConnection());
         HashMap<Integer,Attack> allattacks = dbh.getAllAttacks();
         int num_atk = 0;
@@ -75,11 +76,12 @@ public class HelloController implements Initializable {
                 AttackContainer ac = fxmlLoader.getController();
                 int type = AttackHelper.attackModuleToInt(atk.getAttackModule());
                 ac.Setlabels(atk.getName(),String.valueOf(atk.getDamage()),String.valueOf(atk.getSpeed()),choices[type-1]);
-                gpAttackCont.add(sc,num_atk%contcol,num_atk/contcol);
+                gp.add(sc,num_atk%contcol,num_atk/contcol);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
             num_atk++;
         }
+        spContainer.setContent(gp);
     }
 }
